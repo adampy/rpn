649a7a5638e50004ab27b25763a4c6321a543820
Adam Crawley <37088720+adampy@users.noreply.github.com>
2020-08-06 23:30:58 +0100

    (-) Random pointer tests

--- RPN/Program.cs
+++ RPN/Program.cs
@@ -67,25 +67,11 @@ namespace RPN
         }
         static void Main(string[] args)
         {
-            int[] arr = { 4, 7, 7, 27, 3, 1, 2, 89, 7, 1 };
-
-            unsafe
-            {
-                fixed (int* pointer = &arr[0])
-                {
-                    Console.WriteLine(*pointer);
-                    int inc = Convert.ToInt32(Console.ReadLine());
-                    int* pointer2 = pointer + inc;
-                    Console.WriteLine(*pointer2);
-                }
-            }
-            
-
-            /*Console.Write("Enter a postfix notation expression: ");
+            Console.Write("Enter a postfix notation expression: ");
             string input = Console.ReadLine();
             string[] splitted = input.Split(' ');
 
-            Console.WriteLine(ReversePolish(splitted));*/
+            Console.WriteLine(ReversePolish(splitted));
             
         }
     }
